219b5604080db3b038a032a83adf6b440ffef739
haozibi <haozibi@foxmail.com>
2016-01-10 15:09:27 +0800

    0.0

--- python3.5.1.centos.all.sh
+++ python3.5.1.centos.all.sh
@@ -6,12 +6,12 @@ wget https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz
 tar -zxvf Python-3.5.1.tgz
 cd Python-3.5.1
 ./configure && make
-make install 
+make install
 ln -s /usr/local/bin/python3  /usr/bin/python3
 
 clear
 
 echo Python3 successful installation,enjoy it
-echo python => python2
-echo python3 = > python3
+echo " python --> python2 "
+echo " python3 --> python3 "
 echo Welcome to www.bzizi.com -- haozibi
